aff2fe9630c4fda5400a64487522b54e1b669a03
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-13 13:37:37 -0800

    finish test for computerThrow, set up some other functions

--- test/example.test.js
+++ test/example.test.js
@@ -6,18 +6,18 @@ const test = QUnit.test;
 //computerThrow function test
 test(' this function will given 0 return rock, given 1 return paper, given 2 return scissors', (expect) => {
    
-    const expected0 = true;
-    const actual0 = false;
+    const expected0 = 'rock';
+    const actual0 = computerThrow(0);
 
     expect.equal(actual0, expected0);
 
-    const expected1 = true;
-    const actual1 = false;
+    const expected1 = 'paper';
+    const actual1 = computerThrow(1);
     
     expect.equal(actual1, expected1);
 
-    const expected2 = true;
-    const actual2 = false;
+    const expected2 = 'scissors';
+    const actual2 = computerThrow(2);
     
     expect.equal(actual2, expected2);
 });
---
+++ utils.js
@@ -0,0 +1,6 @@
+export function computerThrow(throwNum) {
+    if (throwNum === 0) return 'rock';
+    if (throwNum === 1) return 'paper';
+    if (throwNum === 2) return 'scissors';
+
+}
