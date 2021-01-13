d1fde9d3368284aa5a320015bb8cd54b0220d90d
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-13 13:54:18 -0800

    add html skeleton and boiler plate js

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-// import functions and grab DOM elements
+import {computerThrow} from './utils.js';
+import {getRandomThrow} from './get-random-throw.js';
+
+const throwButton = document.getElementById('throw-button');
+
+const winsSpan = document.getElementById('wins-span');
+const lossesSpan = document.getElementById('losses-span');
+const totalSpan = document.getElementById('total-span');
+
+const retryButton = document.getElementById('retry-button');
+
+const opponentSelectionImg = document.getElementById('opponent-selection-img');
 
 // initialize state
+let wins = 0;
+let total = 0;
 
 // set event listeners to update state and DOM
+
+throwButton.addEventListener('click', () => {
+
+}); 
+
+retryButton.addEventListener('click', () => {
+
+}); 
